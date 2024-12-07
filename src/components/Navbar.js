802c7ac18f039ac47663469f0a64655705dbaf14
Ritesh <riteshtiwari3112003@gmail.com>
2024-12-07 23:36:28 +0530

    this is my two hundred seventy five commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>
-        <nav>
+        <nav className="bg-bgDark py-4">
             <h1>Top Course</h1>
         </nav>
       
